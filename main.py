75034dc8cd3d3d67081c6e8f2be06e051f8bcc2d
Christian Silver <christian@decode.org.nz>
2017-04-29 13:15:08 +0100

    Add new config parameters

--- main.py
+++ main.py
@@ -3,14 +3,22 @@ import sim
 import csv_sim
 
 CONFIG = sim.Config(
-    death_rate=0.1,
-    birth_chance=0.5,
+    # The chance that, each year, any given animal will die
+    death_rate=0.2,
+    # The chance that a female has of giving birth each year
+    birth_chance=1,
+    # The chance that a new male will die
+    male_birth_mortality=0.2,
+    # The chance that a new female will diea
+    female_birth_mortality=0.2,
+    # The max age of an animal before it "dies of old age"
+    max_age=20
 )
 
-START_ANIMALS = sim.create_babies(50)
+START_ANIMALS = sim.create_babies(25, 'f') + sim.create_babies(25, 'm')
 
 ## Uncomment out to draw the sim
-# draw_sim.run_and_draw_sim(START_ANIMALS, CONFIG)
+draw_sim.run_and_draw_sim(START_ANIMALS, CONFIG)
 
 ## Uncomment out to run the sim and save to csv
-csv_sim.run_and_save_sim(START_ANIMALS, CONFIG, years=100, filename="sim.csv")
+# csv_sim.run_and_save_sim(START_ANIMALS, CONFIG, years=100, filename="sim.csv")
--- sim.py
+++ sim.py
@@ -4,26 +4,40 @@ from random import random, choice
 FIELD_SIZE = 100
 
 Animal = namedtuple('Animal', ['x', 'y', 'sex', 'age'])
-Config = namedtuple('Config', ['death_rate', 'birth_chance'])
+Config = namedtuple('Config', [
+    'male_birth_mortality',
+    'female_birth_mortality',
+    'death_rate',
+    'birth_chance',
+    'max_age',
+])
 
-GENDERS = ['m', 'f']
+SEXES = ['m', 'f']
 
-def random_animal():
-    return Animal(x=FIELD_SIZE * random(), y=FIELD_SIZE * random(), sex=choice(GENDERS), age=0)
+def random_animal(sex = None):
+    if sex == None:
+        sex = choice(SEXES)
+    return Animal(x=FIELD_SIZE * random(), y=FIELD_SIZE * random(), sex=sex, age=0)
 
-def create_babies(count):
-    return [random_animal() for _ in range(count)]
+def create_babies(count, sex):
+    return [random_animal(sex=sex) for _ in range(count)]
 
 def step_sim(animals, config):
-    def kill_animals(animal):
-        return filter(lambda _: random() > config.death_rate, animals)
+    def should_animal_live(animal):
+        return (random() > config.death_rate) and (animal.age < config.max_age)
+
+    def kill_animals(animals):
+        return filter(should_animal_live, animals)
 
     new_animals = []
 
     for animal in kill_animals(animals):
-
         if (animal.sex == 'f') and (animal.age >= 2) and (random() < config.birth_chance):
-            new_animals.append(random_animal())
+            new_animal = random_animal()
+            death_roll = random()
+            if (((death_roll > config.male_birth_mortality) and (new_animal.sex == 'm')) or
+                    ((death_roll > config.female_birth_mortality) and (new_animal.sex == 'f'))):
+                new_animals.append(new_animal)
 
         new_animals.append(animal._replace(age=animal.age + 1))
 
